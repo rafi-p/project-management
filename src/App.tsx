c8037812de4b8a7f02c86b0af716af47bce6d555
Rafi <mcintoshhd@Mcintoshs-MacBook-Air.local>
2023-05-28 22:47:23 +0700

    add sidebar component and css

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,13 @@ import Login from './pages/login/Login';
 import Project from './pages/project/Project';
 import Signup from './pages/signup/Signup';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Sidebar />
         <div className="container">
           <Navbar />
           <Routes>
