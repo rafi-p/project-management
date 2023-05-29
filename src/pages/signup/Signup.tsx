cc184dc68a297562570d4200a2ee42bcd431a232
Rafi <mcintoshhd@Mcintoshs-MacBook-Air.local>
2023-05-29 09:58:44 +0700

    Add form which include email, password, display name, and file upload Add validation required on each input Add validation for file that only accept image and file below 100kb Add submit button

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,11 +1,88 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
 // styles
 import './Signup.css'
 
 export default function Signup() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [displayName, setDisplayName] = useState('')
+    const [thumbnail, setThumbnail] = useState<File | null>(null)
+    const [thumbnailError, setThumbnailError] = useState('')
+
+
+    const handleFileChange = (e: ChangeEvent) => {
+        setThumbnail(null)
+        const target = e.target as HTMLInputElement
+        let selected = (target.files as FileList)[0]
+
+        if(!selected) {
+            setThumbnailError('Please select a file')
+            return
+        }
+        if(!selected.type.includes('image')) {
+            setThumbnailError('Selected file must be an image')
+            return
+        }
+        if(selected.size > 100000) {
+            setThumbnailError('Image file size must be less then 100kb')
+            return
+        }
+
+        setThumbnailError('')
+        setThumbnail(selected)
+
+    }
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+    }
+
 
     return (
-        <div>
-            Signup
-        </div>
+        <form 
+            className='auth-form'
+            onSubmit={handleSubmit}
+        >
+            <h2>Sign up</h2>
+            <label>
+                <span>email:</span>
+                <input 
+                    required 
+                    type="email" 
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+            </label>
+            <label>
+                <span>password:</span>
+                <input 
+                    required 
+                    type="password" 
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </label>
+            <label>
+                <span>display name:</span>
+                <input 
+                    required 
+                    type="text" 
+                    onChange={(e) => setDisplayName(e.target.value)}
+                    value={displayName}
+                />
+            </label>
+            <label>
+                <span>profile thumbnail:</span>
+                <input 
+                    required 
+                    type="file" 
+                    onChange={handleFileChange}
+                />
+                {thumbnailError && <div className='error'>{thumbnailError}</div>}
+            </label>
+            <button className="btn">
+                Sign up
+            </button>
+        </form>
     )
 }
